f8dd4fa5a84f54b5db8279ea3fe123b7a12f7e7d
Jorge Correa <jorge.correa@innocv.com>
2017-08-12 16:07:29 +0200

    fixed with recommend serialization pattern
    https://sonarcloud.io/project/issues?id=business-template%3Amaster&issues=AV3WEaaupvRVEt3bythB&open=AV3WEaaupvRVEt3bythB

--- src/Domain/DomainException.cs
+++ src/Domain/DomainException.cs
@@ -1,22 +1,25 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Domain
 {
-	public class DomainException
-		: Exception
-	{
-		public DomainException()
-		{
-		}
+    [Serializable]
+    public class DomainException
+        : Exception
+    {
+        public DomainException()
+        { }
 
-		public DomainException(string message)
-			: base(message)
-		{
-		}
+        public DomainException(string message)
+            : base(message)
+        { }
 
-		public DomainException(string message, Exception inner)
-			: base(message, inner)
-		{
-		}
-	}
+        public DomainException(string message, Exception inner)
+            : base(message, inner)
+        { }
+
+        protected DomainException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
+    }
 }
